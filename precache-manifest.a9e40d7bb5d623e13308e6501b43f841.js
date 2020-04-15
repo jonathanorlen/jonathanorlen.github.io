179a5e2384b1446e85316f322d147486982ce5c1
jonathan orlen pellonodu <wahyu.orlen@hotmail.com>
2020-04-15 21:20:33 +0700

    Menyalin isi folder build

---
+++ precache-manifest.a9e40d7bb5d623e13308e6501b43f841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe572c37aa9dc3e17bb87bec8e366030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f82ffdc26a752f78235",
+    "url": "/static/css/main.4a1214df.chunk.css"
+  },
+  {
+    "revision": "121deab8a0fd3f88a661",
+    "url": "/static/js/2.1f127285.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f127285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f82ffdc26a752f78235",
+    "url": "/static/js/main.2adfebca.chunk.js"
+  },
+  {
+    "revision": "4983923bfbda8f01423b",
+    "url": "/static/js/runtime-main.b3416acf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
